b401796a1345b16149e3af50cc08795879ab3677
Steeve MOKOKO <steeve.mokoko14@gmail.com>
2024-07-13 00:09:14 +0200

    Correction erreurs RecyclerView

--- app/src/main/java/com/openclassrooms/tajmahal/ui/restaurant/recyclerView/ReviewAdapter.java
+++ app/src/main/java/com/openclassrooms/tajmahal/ui/restaurant/recyclerView/ReviewAdapter.java
@@ -11,12 +11,9 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.RequestManager;
 import com.openclassrooms.tajmahal.R;
-import com.openclassrooms.tajmahal.ReviewsFragment;
 import com.openclassrooms.tajmahal.domain.model.Review;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder> {
@@ -27,59 +24,41 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewView
         this.reviewList = reviewList;
     }
 
-
-    // RETURN THE TOTAL COUNT OF ITEMS IN THE LIST
+    @NonNull
     @Override
-    public int getItemCount() {
-        return this.reviewList.size();
-    }
-
-    @Override
-    public ReviewViewHolder onCreatViewHolder(ViewGroup parent, int viewType) {
-        // HERE WE CREATE VIEW HOLDER AND INFLATE HIS IT XML LAYOUT
+    public ReviewViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.item_reviews, parent, false);
-
         return new ReviewViewHolder(view);
     }
 
-    @NonNull
-    @Override
-    public ReviewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    // UPDATE VIEW HOLDER WITH A NEW INFO (REVIEW)
     @Override
     public void onBindViewHolder(ReviewViewHolder viewHolder, int position) {
         viewHolder.updateReview(this.reviewList.get(position));
     }
 
+    @Override
+    public int getItemCount() {
+        return this.reviewList.size();
+    }
 
-
-
-    public class ReviewViewHolder extends RecyclerView.ViewHolder {
+    public static class ReviewViewHolder extends RecyclerView.ViewHolder {
 
         TextView username;
         ImageView imageUser;
 
         public ReviewViewHolder(View itemView) {
             super(itemView);
-
             username = itemView.findViewById(R.id.userName);
             imageUser = itemView.findViewById(R.id.profileUser);
-
-            Review review;
-            View context;
-            Glide.with(context)
-                    .load(review.getPicture())
-                    .into(this.imageUser);
         }
 
         public void updateReview(Review review) {
             this.username.setText(review.getUsername());
+            Glide.with(itemView.getContext())
+                    .load(review.getPicture())
+                    .into(this.imageUser);
         }
-
     }
 }
